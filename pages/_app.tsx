f730d7efa02a94edf069b263f6253ac7828603fe
Ariel Saldana <ariel@unreal.io>
2022-12-12 21:07:39 -0500

    Commit changes 🥳

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return(
+      <main data-scroll-container className="container">
+        <Component {...pageProps} />
+      </main>
+  )
 }
